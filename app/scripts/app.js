169c2e2e1f7f2841e70ffa21878d8b9371cc6a83
El Juli <mayorga.julian@gmail.com>
2014-04-14 11:06:37 -0300

    feat(views/main): add navbar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('inmobiliariaApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'mgcrea.ngStrap'
 ])
   .config(function ($routeProvider) {
     $routeProvider
